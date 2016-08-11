710660718ba69200e1d75c3b5a8ca06cbb224853
Lilian Kasem <liliankasem@gmail.com>
2016-08-11 16:40:30 +0100

    Implemented API endpoint for StopPoint
    Implements API endpoint as supported by [TFL
    StopPoint](https://api.tfl.gov.uk/#StopPoint)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ module.exports = function (appId, appKey) {
     accidentstats: require('./accidentstats')(appId, appKey),
     place: require('./place')(appId, appKey),
     occupancy: require('./occupancy')(appId, appKey),
-    journey: require('./journey')(appId, appKey)
+    journey: require('./journey')(appId, appKey),
+    stoppoint: require('./stoppoint')(appId, appKey)
   };
 
 }
